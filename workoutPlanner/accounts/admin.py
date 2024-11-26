b6d932e6da18198638df6af20c628090005d1d87
miftari1 <miftarib@hotmail.com>
2024-11-26 20:45:42 +0100

    Customized CustomUserAdmin using fieldsets option

--- workoutPlanner/accounts/admin.py
+++ workoutPlanner/accounts/admin.py
@@ -3,6 +3,19 @@ from django.contrib import admin
 from workoutPlanner.accounts.models import CustomUser, Profile
 
 
-@admin.register(CustomUser, Profile)
+@admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = [
+        (
+            'Credentials',
+            {'fields': ['username','email', 'password']},
+        ),
+        (
+            'Groups and Permissions',
+            {'fields': ['groups', 'user_permissions']}
+        ),
+        (
+            'Status',
+            {'fields': ['is_superuser', 'is_staff', 'is_active']}
+        )
+    ]
