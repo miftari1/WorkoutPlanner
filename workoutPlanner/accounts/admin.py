1d815eff8cfb8139ea406796f846a74ddde1fbdb
miftari1 <miftarib@hotmail.com>
2024-11-26 21:27:44 +0100

    Modified Profile and CustomUser admin pages

--- workoutPlanner/accounts/admin.py
+++ workoutPlanner/accounts/admin.py
@@ -5,6 +5,10 @@ from workoutPlanner.accounts.models import CustomUser, Profile
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_superuser', 'is_staff', 'is_active']
+    list_filter = ['is_active', 'is_superuser', 'is_staff']
+    search_fields = ('username', 'email')
+    ordering = ('is_superuser', 'is_staff', 'is_active')
     fieldsets = [
         (
             'Credentials',
@@ -17,5 +21,37 @@ class CustomUserAdmin(admin.ModelAdmin):
         (
             'Status',
             {'fields': ['is_superuser', 'is_staff', 'is_active']}
+        )
+    ]
+    show_facets = admin.ShowFacets.ALWAYS
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'age', 'gender', 'goal', 'activity']
+    search_fields = ['user__username', 'user__email']
+    list_filter = ['activity', 'goal', 'gender', 'age']
+    ordering = ['goal', 'activity']
+    exclude = ['calories_needed']
+    fieldsets = [
+        (
+            None,
+            {'fields': ['user']}
+        ),
+        (
+            'Personal Info',
+            {
+                'fields':
+             [
+                 'first_name',
+              'last_name',
+              'gender',
+              'age',
+              'height',
+              'weight',
+              'activity',
+              'goal'
+              ]
+             },
         )
     ]
