b2e3ab4f424105c36bb6bd3c3095361849a1a785
miftari1 <miftarib@hotmail.com>
2024-12-02 23:12:00 +0100

    Modified Post Admin

--- workoutPlanner/forum/admin.py
+++ workoutPlanner/forum/admin.py
@@ -5,4 +5,11 @@ from workoutPlanner.forum.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    show_facets = admin.ShowFacets.ALWAYS
